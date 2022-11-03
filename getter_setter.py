35c297a900154a2e9a13ccbb5a8cd96bf569727b
jeonhyoungmin <jhmin7509@gmail.com>
2022-11-03 18:30:30 +0900

    js, py getter, setter

---
+++ getter_setter.py
@@ -0,0 +1,21 @@
+# python-getter_setter-training
+class Pokemon:
+    def __init__(self, id, name, type):
+        self.id = id
+        self.name = name
+        self.type = type
+    @property
+    def id(self):
+        return self.__id
+    @id.setter
+    def id(self, id):
+        print(str(type(id)))
+        #  <class 'float'>
+        if str(type(id)) == "<class 'int'>":
+            self.__id = id
+        else:
+            print("id 타입은 정수여야 합니다.")
+# class 선언
+pikachu = Pokemon(3.3, '피카츄', '전기')
+print(pikachu.__dict__)
+# print()에는 무슨 값이 나올까요?
